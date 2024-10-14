aa62e0ff4c0d5bafdaa2602824f3587a89d48513
Rivio98 <rivaldogjoni1998@gmail.com>
2024-10-14 15:54:25 +0200

    [UPD]Milestone 1 completed

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class UserController extends Controller
+{
+    public function ProjectIndex()
+    {
+        $projects = Project::all();
+        return response()->json($projects);
+    }
+}
