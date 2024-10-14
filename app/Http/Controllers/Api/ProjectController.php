48d29d02f0b68d630f63eb11ee6a489964e0d020
Rivio98 <rivaldogjoni1998@gmail.com>
2024-10-14 16:23:17 +0200

    [UPD]Milestone 2 complete

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
 
-class UserController extends Controller
+class ProjectController extends Controller
 {
-    public function ProjectIndex()
+    public function index()
     {
         $projects = Project::all();
         return response()->json($projects);
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\ProjectController as ProjectController;
+use App\Http\Controllers\Api\ProjectController as ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/ProjectIndedx', [ProjectController::class, 'home'])->name('project_home');
+Route::get('/index', [ProjectController::class, 'index'])->name('project_home');
